3756e38e3a02442fc5d5525469f112b6f2587014
mamanggim <mamanggim@gmail.com>
2025-04-28 08:25:10 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,19 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDi5nCsLUOQNhPG6Bnxgsw8W60ZPaQewgw",
+  authDomain: "harvest-pi.firebaseapp.com",
+  databaseURL: "https://harvest-pi-default-rtdb.firebaseio.com",
+  projectId: "harvest-pi",
+  storageBucket: "harvest-pi.firebasestorage.app",
+  messagingSenderId: "650006770674",
+  appId: "1:650006770674:web:bf6291198fc0a02be7b16b"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const auth = firebase.auth();
+
+// Export untuk module (jika pakai module)
+window.database = database;
+window.auth = auth;

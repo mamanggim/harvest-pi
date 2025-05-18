422a8b404f57d320d8c6c5590b675c274d281513
mamanggim <mamanggim@gmail.com>
2025-05-18 18:14:34 +0700

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDi5nCsLUOQNhPG6Bnxgsw8W60ZPaQewgw",
+  authDomain: "harvest-pi.firebaseapp.com",
+  projectId: "harvest-pi",
+  storageBucket: "harvest-pi.firebasestorage.app",
+  messagingSenderId: "650006770674",
+  appId: "1:650006770674:web:bf6291198fc0a02be7b16b",
+  measurementId: "YOUR_MEASUREMENT_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = 'New Deposit Request';
+  const notificationOptions = {
+    body: `User: ${payload.data.userId}, Amount: ${payload.data.amount} PI`,
+    icon: '/assets/img/ui/water_icon.png', // Icon notifikasi
+    click_action: '/admin-panel'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

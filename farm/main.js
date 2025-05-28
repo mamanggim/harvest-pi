a7e096fb85e78a674dab813c3b10193372270bec
mamanggim <mamanggim@gmail.com>
2025-05-28 11:34:13 +0700

    Update main.js

--- farm/main.js
+++ farm/main.js
@@ -455,12 +455,6 @@ function updateReferralLink() {
 }
 
 // Listener untuk LOGIN
-const loginEmailBtn = document.getElementById('login-email-btn');
-const emailInput = document.getElementById('email-input');
-const passwordInput = document.getElementById('password-input');
-const loginError = document.getElementById('login-error');
-const verifyEmailMsg = document.getElementById('verify-email-message');
-
 if (loginEmailBtn) {
   addSafeClickListener(loginEmailBtn, async (e) => {
     e.preventDefault();

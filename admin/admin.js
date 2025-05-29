eecc28842d48e38e3b6879ac657ae61ab7d4137f
mamanggim <mamanggim@gmail.com>
2025-05-29 12:32:27 +0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,6 +1,23 @@
-import { auth, database, ref, onValue, set, get, update } from '/firebase/firebase-config.js';
-import { addSafeClickListener } from '/main.js'; // Sesuain path
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getDatabase, ref, onValue, set, get, update } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js';
+import { addSafeClickListener } from '../main.js';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDi5nCsLUOQNhPG6Bnxgsw8W60ZPaQewgw",
+  authDomain: "harvest-pi.firebaseapp.com",
+  databaseURL: "https://harvest-pi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "harvest-pi",
+  storageBucket: "harvest-pi.firebasestorage.app",
+  messagingSenderId: "650006770674",
+  appId: "1:650006770674:web:bf6291198fc0a02be7b16b",
+  measurementId: "G-HV6J072QQZ"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+// ... (sisa kode admin.js)
 document.addEventListener('DOMContentLoaded', () => {
   let isLoggingOut = false;
   let lastNotificationTime = 0;

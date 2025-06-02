24f1708554b79622f098aae819bf6c2e54f07e22
mamanggim <mamanggim@gmail.com>
2025-06-02 10:33:29 +0700

    Update inventory.js

--- features/inventory.js
+++ features/inventory.js
@@ -1,2 +1,6 @@
 export function addToInventory(type, veg, qty = 1) { ... }
 export function renderInventory() { ... }
+import {
+  getInventory, setInventory,
+  getLangData, getCurrentLang
+} from '/core/global-state.js';

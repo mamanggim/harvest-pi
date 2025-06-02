45ca67363539edb52fbb02deabc367643898881c
mamanggim <mamanggim@gmail.com>
2025-06-02 10:32:13 +0700

    Update shop.js

--- features/shop.js
+++ features/shop.js
@@ -1,2 +1,9 @@
 export function renderShop() { ... }
 export async function buyVegetable(id, currency) { ... }
+import {
+  getFarmCoins, setFarmCoins,
+  getPiBalance, setPiBalance,
+  getWater, setWater,
+  getLangData, getCurrentLang,
+  getVegetables, getInventory, setInventory
+} from '/core/global-state.js';

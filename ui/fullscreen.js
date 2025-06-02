909c3da95b17984227c7a2d8fcd5419ccc2dcf85
mamanggim <mamanggim@gmail.com>
2025-06-02 13:38:27 +0700

    Update fullscreen.js

--- ui/fullscreen.js
+++ ui/fullscreen.js
@@ -50,3 +50,18 @@ export function exitFullScreen() {
     document.msExitFullscreen();
   }
 }
+
+export function enterFullScreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) elem.requestFullscreen();
+  else if (elem.mozRequestFullScreen) elem.mozRequestFullScreen();
+  else if (elem.webkitRequestFullscreen) elem.webkitRequestFullscreen();
+  else if (elem.msRequestFullscreen) elem.msRequestFullscreen();
+}
+
+export function exitFullScreen() {
+  if (document.exitFullscreen) document.exitFullscreen();
+  else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+  else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+  else if (document.msExitFullscreen) document.msExitFullscreen();
+}

3a144b2f1ae77b1a3cda2796f32f43c160a21365
mamanggim <mamanggim@gmail.com>
2025-06-02 08:45:21 +0700

    Update register.js

--- auth/register.js
+++ auth/register.js
@@ -1,1 +1,5 @@
-
+const registerEmailBtn = document.getElementById('register-email-btn');
+const registerEmailInput = document.getElementById('register-email-input');
+const registerPasswordInput = document.getElementById('register-password-input');
+const registerUsernameInput = document.getElementById('register-username-input');
+const registerError = document.getElementById('register-error');

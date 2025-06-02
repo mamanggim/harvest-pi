1839026ab8a6f4c272b2e16a98191381b3bb3951
mamanggim <mamanggim@gmail.com>
2025-06-02 08:46:52 +0700

    Update login.js

--- auth/login.js
+++ auth/login.js
@@ -1,1 +1,5 @@
-
+const loginEmailBtn = document.getElementById('login-email-btn');
+const emailInput = document.getElementById('email-input');
+const passwordInput = document.getElementById('password-input');
+const loginError = document.getElementById('login-error');
+const verifyEmailMsg = document.getElementById('verify-status');

a3b337ed7cfe5aba090dd21fd1ec553d2c5c7079
mamanggim <mamanggim@gmail.com>
2025-06-02 08:57:59 +0700

    Update session.js

--- auth/session.js
+++ auth/session.js
@@ -1,1 +1,9 @@
+export function switchToLogin() {
+  document.getElementById('login-screen').style.display = 'flex';
+  document.getElementById('register-screen').style.display = 'none';
+}
 
+export function switchToRegister() {
+  document.getElementById('login-screen').style.display = 'none';
+  document.getElementById('register-screen').style.display = 'flex';
+}

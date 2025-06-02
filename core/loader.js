9c9ab8557e19568a9ea8e8cb2cb960f387f5f78d
mamanggim <mamanggim@gmail.com>
2025-06-02 08:14:19 +0700

    Update loader.js

--- core/loader.js
+++ core/loader.js
@@ -1,1 +1,19 @@
+import { setLangData, setVegetables } from './global-state.js';
+import { showNotification } from '/ui/notification.js';
 
+export async function loadData() {
+  try {
+    const langRes = await fetch('/data/lang.json');
+    const langJson = await langRes.json();
+    setLangData(langJson);
+    console.log('Language data loaded:', langJson);
+
+    const vegRes = await fetch('/data/vegetables.json');
+    const vegJson = await vegRes.json();
+    setVegetables(vegJson.vegetables);
+    console.log('Vegetables data loaded:', vegJson.vegetables);
+  } catch (error) {
+    console.error('Error loading data:', error.message);
+    showNotification('Error loading game data.');
+  }
+}

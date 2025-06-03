b9b428ebee0064c50623a945b948d486f2ec5af8
mamanggim <mamanggim@gmail.com>
2025-06-04 06:14:52 +0700

    Update main.js

--- farm/main.js
+++ farm/main.js
@@ -1,8 +1,8 @@
-import { initializeGame } from '/core/init.js'; // ini versi final yang udah terintegrasi semua
-import { updateVolumes, initAudioControls } from '/ui/volume-control.js';
-import { loadExchangeRate } from '/features/exchange.js';
-import { setupGlobalEventHandlers } from '/ui/event-bindings.js';
-import { setupStartGameHandler } from '/ui/start-handler.js';
+import { initializeGame } from '../core/init.js'; // ini versi final yang udah terintegrasi semua
+import { updateVolumes, initAudioControls } from '../ui/volume-control.js';
+import { loadExchangeRate } from '../features/exchange.js';
+import { setupGlobalEventHandlers } from '../ui/event-bindings.js';
+import { setupStartGameHandler } from '../ui/start-handler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initAudioControls();
